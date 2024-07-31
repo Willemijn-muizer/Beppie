8e1f3d1e5a61c92029739e257bf92495282a6968
Willemijn-muizer <willemijn.muizer@hotmail.com>
2024-07-31 16:20:50 +0200

    Bland-Altman plot participants categorized by labels.

--- inner_study/main.py
+++ inner_study/main.py
@@ -54,8 +54,7 @@ def calculate_std_differences(delta_col: pd.Series, print_name: str) -> float:
 
 
 def plot_bland_altman_willemijn_script(df: pd.DataFrame):
-    x = (df['mean_intra'] + df['Script']) / 2
-    y = df['delta']
+    groups = df.groupby('Participant')
 
     std_difference = calculate_std_differences(df["delta"], "Willemijn & script")
 
@@ -63,31 +62,38 @@ def plot_bland_altman_willemijn_script(df: pd.DataFrame):
     upper_limit = mean_difference + 1.96 * std_difference
     lower_limit = mean_difference - 1.96 * std_difference
 
-    plt.title("Bland Altman plot with data Willemijn and Script")
-    plt.xlabel("Average between observer and script")
-    plt.ylabel("Difference between observer and script")
-    plt.scatter(x, y)
+    fig, ax = plt.subplots(figsize=(15, 10))
+
+    ax.set_title("Bland Altman plot with data Willemijn and Script")
+    ax.set_xlabel("Average between observer and script")
+    ax.set_ylabel("Difference between observer and script")
+
+    for name, group in groups:
+        xs = (group['mean_intra'] + group['Script']) / 2
+        ys = group['delta']
+
+        ax.scatter(xs, ys, marker='o', label=name)
 
     # Draw a line at  mean_difference, upper_limit, lower_limit
-    plt.axhline(mean_difference, color='b')
-    plt.text(30, mean_difference - 0.25, "Mean=" + str(mean_difference)[:5], color="b")
+    ax.axhline(mean_difference, color='b')
+    ax.text(30, mean_difference - 0.15, "Mean=" + str(mean_difference)[:5], color="b")
 
-    plt.axhline(upper_limit, color='r', linestyle="--")
-    plt.text(2, upper_limit + 0.1, "+1.96 SD; " + str(upper_limit)[:5], color="r")
+    ax.axhline(upper_limit, color='r', linestyle="--")
+    ax.text(2, upper_limit + 0.1, "+1.96 SD; " + str(upper_limit)[:5], color="r")
 
-    plt.axhline(lower_limit, color='r', linestyle="--")
-    plt.text(2, lower_limit + 0.1, "-1.96 SD; " + str(lower_limit)[:5], color="r")
+    ax.axhline(lower_limit, color='r', linestyle="--")
+    ax.text(2, lower_limit + 0.1, "-1.96 SD; " + str(lower_limit)[:5], color="r")
 
-    plt.text(28, 2.70, "Participant 10, \n RB, Sessie 2")
 
-    plt.savefig('bland_altman_willemijn_script.png')
+    ax.legend(loc="lower right")
+
+    fig.savefig('bland_altman_willemijn_script.png')
     plt.close()
     plt.clf()
 
 
 def plot_bland_altman_irma_script(df: pd.DataFrame):
-    x = (df['Meting 1'] + df['Script']) / 2
-    y = df['delta']
+    groups = df.groupby('Participant')
 
     std_difference = calculate_std_differences(df["delta"], "Irma & script")
 
@@ -95,22 +101,31 @@ def plot_bland_altman_irma_script(df: pd.DataFrame):
     upper_limit = mean_difference + 1.96 * std_difference
     lower_limit = mean_difference - 1.96 * std_difference
 
-    plt.title("Bland Altman plot with data Irma and Script")
-    plt.xlabel("Average between observer and script")
-    plt.ylabel("Difference between observer and script")
-    plt.scatter(x, y)
+    fig, ax = plt.subplots(figsize=(15, 10))
+
+    ax.set_title("Bland Altman plot with data Irma and Script")
+    ax.set_xlabel("Average between observer and script")
+    ax.set_ylabel("Difference between observer and script")
+
+    for name, group in groups:
+        xs = (group['Meting 1'] + group['Script']) / 2
+        ys = group['delta']
+
+        ax.scatter(xs, ys, marker='o', label=name)
 
     # Draw a line at  mean_difference, upper_limit, lower_limit
-    plt.axhline(mean_difference, color='b')
-    plt.text(30, mean_difference - 0.25, "Mean=" + str(mean_difference)[:5], color="b")
+    ax.axhline(mean_difference, color='b')
+    ax.text(30, mean_difference - 0.25, "Mean=" + str(mean_difference)[:5], color="b")
 
-    plt.axhline(upper_limit, color='r', linestyle="--")
-    plt.text(2, upper_limit + 0.1, "+1.96 SD; " + str(upper_limit)[:5], color="r")
+    ax.axhline(upper_limit, color='r', linestyle="--")
+    ax.text(2, upper_limit + 0.1, "+1.96 SD; " + str(upper_limit)[:5], color="r")
 
-    plt.axhline(lower_limit, color='r', linestyle="--")
-    plt.text(2, lower_limit + 0.1, "-1.96 SD; " + str(lower_limit)[:5], color="r")
+    ax.axhline(lower_limit, color='r', linestyle="--")
+    ax.text(2, lower_limit + 0.1, "-1.96 SD; " + str(lower_limit)[:5], color="r")
 
-    plt.savefig('bland_altman_irma_script.png')
+    ax.legend(loc="lower right")
+
+    fig.savefig('bland_altman_irma_script.png')
     plt.close()
     plt.clf()
 
@@ -118,8 +133,14 @@ def plot_bland_altman_irma_script(df: pd.DataFrame):
 def plot_bland_altman_observators(df_will: pd.DataFrame, df_irma: pd.DataFrame):
     assert len(df_will) == len(df_irma)
 
-    x = (df_will['mean_intra'] + df_irma['Meting 1']) / 2
     y = df_will['mean_intra'] - df_irma['Meting 1']
+
+    df = pd.DataFrame()
+    df['Participant'] = df_will["Participant"]
+    df['mean_intra'] = df_will['mean_intra']
+    df['Meting 1'] = df_irma['Meting 1']
+
+    groups = df.groupby('Participant')
 
     std_difference = calculate_std_differences(y, "Willemijn & Irma")
 
@@ -127,20 +148,29 @@ def plot_bland_altman_observators(df_will: pd.DataFrame, df_irma: pd.DataFrame):
     upper_limit = mean_difference + 1.96 * std_difference
     lower_limit = mean_difference - 1.96 * std_difference
 
-    plt.title("Bland Altman plot with data Willemijn and Irma")
-    plt.xlabel("Average between two observers")
-    plt.ylabel("Difference between two observers")
-    plt.scatter(x, y)
+    fig, ax = plt.subplots(figsize=(15, 10))
+
+    ax.set_title("Bland Altman plot with data Willemijn and Irma")
+    ax.set_xlabel("Average between two observers")
+    ax.set_ylabel("Difference between two observers")
+
+    for name, group in groups:
+        xs = (group['mean_intra'] + group['Meting 1']) / 2
+        ys = group['mean_intra'] - group['Meting 1']
+
+        ax.scatter(xs, ys, marker='o', label=name)
 
     # Draw a line at  mean_difference, upper_limit, lower_limit
-    plt.axhline(mean_difference, color='b')
-    plt.text(30, mean_difference - 0.25, "Mean=" + str(mean_difference)[:5], color="b")
+    ax.axhline(mean_difference, color='b')
+    ax.text(30, mean_difference, "Mean=" + str(mean_difference)[:5], color="b")
 
-    plt.axhline(upper_limit, color='r', linestyle="--")
-    plt.text(2, upper_limit + 0.1, "+1.96 SD; " + str(upper_limit)[:5], color="r")
+    ax.axhline(upper_limit, color='r', linestyle="--")
+    ax.text(2, upper_limit + 0.1, "+1.96 SD; " + str(upper_limit)[:5], color="r")
 
-    plt.axhline(lower_limit, color='r', linestyle="--")
-    plt.text(2, lower_limit + 0.1, "-1.96 SD; " + str(lower_limit)[:5], color="r")
+    ax.axhline(lower_limit, color='r', linestyle="--")
+    ax.text(2, lower_limit + 0.1, "-1.96 SD; " + str(lower_limit)[:5], color="r")
+
+    ax.legend(loc="lower right")
 
     plt.savefig('bland_altman_observers.png')
     plt.close()
@@ -149,8 +179,14 @@ def plot_bland_altman_observators(df_will: pd.DataFrame, df_irma: pd.DataFrame):
 def plot_bland_altman_scripts(df_will: pd.DataFrame, df_irma: pd.DataFrame):
     assert len(df_will) == len(df_irma)
 
-    x = (df_will['Script'] + df_irma['Script']) / 2
     y = df_will['Script'] - df_irma['Script']
+
+    df = pd.DataFrame()
+    df['Participant'] = df_will["Participant"]
+    df['Script_will'] = df_will['Script']
+    df['Script_irma'] = df_irma['Script']
+
+    groups = df.groupby('Participant')
 
     std_difference = calculate_std_differences(y, "Script_Will & Script_Irma")
 
@@ -158,20 +194,29 @@ def plot_bland_altman_scripts(df_will: pd.DataFrame, df_irma: pd.DataFrame):
     upper_limit = mean_difference + 1.96 * std_difference
     lower_limit = mean_difference - 1.96 * std_difference
 
-    plt.title("Bland Altman plot with data scripts")
-    plt.xlabel("Average between two scripts")
-    plt.ylabel("Difference between two scripts")
-    plt.scatter(x, y)
+    fig, ax = plt.subplots(figsize=(15, 10))
+
+    ax.set_title("Bland Altman plot with data scripts")
+    ax.set_xlabel("Average between two scripts")
+    ax.set_ylabel("Difference between two scripts")
+
+    for name, group in groups:
+        xs = (group['Script_will'] + group['Script_irma']) / 2
+        ys = group['Script_will'] - group['Script_irma']
+
+        ax.scatter(xs, ys, marker='o', label=name)
 
     # Draw a line at  mean_difference, upper_limit, lower_limit
-    plt.axhline(mean_difference, color='b')
-    plt.text(30, mean_difference - 0.25, "Mean=" + str(mean_difference)[:5], color="b")
+    ax.axhline(mean_difference, color='b')
+    ax.text(30, mean_difference - 0.40, "Mean=" + str(mean_difference)[:5], color="b")
 
-    plt.axhline(upper_limit, color='r', linestyle="--")
-    plt.text(2, upper_limit + 0.1, "+1.96 SD; " + str(upper_limit)[:5], color="r")
+    ax.axhline(upper_limit, color='r', linestyle="--")
+    ax.text(2, upper_limit + 0.1, "+1.96 SD; " + str(upper_limit)[:5], color="r")
 
-    plt.axhline(lower_limit, color='r', linestyle="--")
-    plt.text(2, lower_limit + 0.1, "-1.96 SD; " + str(lower_limit)[:5], color="r")
+    ax.axhline(lower_limit, color='r', linestyle="--")
+    ax.text(2, lower_limit + 0.1, "-1.96 SD; " + str(lower_limit)[:5], color="r")
+
+    ax.legend(loc="lower right")
 
     plt.savefig('bland_altman_scripts.png')
     plt.close()
